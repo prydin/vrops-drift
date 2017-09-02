828871048a66d50a98f69801f04e32d31131fb02
codebox <john@codebox.mylab.dias>
2017-09-02 18:05:06 -0500

    Fixed problems in createSymptom that were throwing 404 and 400 responses (mostly due to use of "data" instead of "json" param in requests Fixed problem with getPropertiesOfObject where resourceKind and adapterKind wereincorrectly entered on query Added input params:    --nowarn to suppress SSL cert warnings from requests    --prefix allows user to specify a different symptom name prefix (Drift is default) Fixed issue where exclude file name was being referenced incorrectly Fixed issue where exclude lines were not being read correctly, throwing a key error

--- mkdriftrules.py
+++ mkdriftrules.py
@@ -7,32 +7,34 @@ baseUrl =''
 headers = { 'Content-Type': 'application/json', 'Accept': 'application/json'}
 
 
+
 def createSymptom(name, severity, adapterKind, resourceKind, property, value, auth):
     payload = {
         'name': name,
         'adapterKindKey': adapterKind,
         'resourceKindKey': resourceKind,
-        'waitCycles': 1,
-        'cancelCycles': 1,
+        'waitCycles': '1',
+        'cancelCycles': '1',
         'state': {
             'severity': severity,
             'condition': {
                 'type': 'CONDITION_PROPERTY_STRING',
-                'stringValue': value,
+                'stringValue': str(value),
                 'operator': 'NOT_EQ',
-                'key': property,
+                'key': str(property),
                 'thresholdType': 'STATIC'
             }
         }
     }
+    print payload
     return requests.post(baseUrl + '/suite-api/api/symptomdefinitions',
                         headers=headers,
                         auth=auth,
                         verify=False,
-                        data=json.dumps(payload))
+                        json=payload)
 
 def getPropertiesOfObject(name, adapterKind, resourceKind, auth):
-    response = requests.get(baseUrl + '/suite-api/api/resources?name={0}&resourceKindKey={1}&adapterKindKey={2}'
+    response = requests.get(baseUrl + '/suite-api/api/resources?name={0}&resourceKind={1}&adapterKind={2}'
                   .format(name, resourceKind, adapterKind),
                   headers=headers,
                   auth=auth,
@@ -53,29 +55,39 @@ parser.add_argument('--user', nargs=1, help='vR Ops username')
 parser.add_argument('--password', nargs=1, help='vR Ops password')
 parser.add_argument('--resource', nargs=1, help='Template resource')
 parser.add_argument('--resourcekind', nargs=1, help='Template resource resource kind')
-parser.add_argument('--exclude', nargs='?', help="Name of exlcude file")
+parser.add_argument('--prefix', nargs=1, help='Symptom name prefix (Drift is default)')
+parser.add_argument('--exclude', nargs='?', help='Name of exlcude file')
+parser.add_argument('--nowarn', default=False, help='Suppress Insecure Request Warning for HTTPS')
 args = parser.parse_args()
+
+if args.nowarn:
+    requests.packages.urllib3.disable_warnings()
 
 # Load exclude file
 #
 excludes = {}
 if args.exclude:
-    for fn in args.exclude:
-        with open(f) as f:
-            for line in f:
-                key = line.strip(' ')
-                excludes[key] = True
+    with open(args.exclude) as f:
+        for line in f:
+            key = line.strip()
+            excludes[key] = True
 
 # Lookup resource and create symptoms based on all non-excluded properties
 #
 baseUrl = 'https://' + args.host[0]
 auth = HTTPBasicAuth(args.user[0], args.password[0])
 result = getPropertiesOfObject(args.resource[0], 'VMWARE', args.resourcekind[0], auth)
+prefix=''
+if args.prefix:
+    prefix=args.prefix[0]
+else:
+    prefix='Drift'
+
 for p in result['property']:
-    pName = p['name']
-    if excludes[pName]:
+    pName = p.get('name')
+    if excludes.get(pName):
+        print pName
         continue
-    result = createSymptom('Drift {0}'.format(pName), 'WARNING', 'VMWARE', args.resource[0],
+    result = createSymptom(prefix + ' {0}'.format(pName), 'WARNING', 'VMWARE', args.resourcekind[0],
                             pName, p['value'], auth)
     print result
-
